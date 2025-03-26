ab91ee5de9aaf88af7b637a435cd758f83dde9d7
huekoh <huexkoh@gmail.com>
2025-03-26 10:47:41 +0800

    Add root layout and constants

---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const Homepage = () => {
+  return <>Store</>;
+};
+
+export default Homepage;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const Homepage = () => {
-  return <Button>Submit</Button>;
-};
-
-export default Homepage;
