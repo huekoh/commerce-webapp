6b19bd30081d755ecd7721a1af8f8f90bcf7af6b
huekoh <huexkoh@gmail.com>
2025-03-27 15:35:02 +0800

    Create page theme toggle for light, dark and system mode

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
-        <div className="wrapper flex-start">
+        <div className="flex-start">
           <Link href="/" className="flex-start">
             <Image
               src="/images/logo.svg"
@@ -17,18 +18,19 @@ const Header = () => {
               height={48}
               priority={true}
             />
-            <span className="hidden lg:block font-bold text2xl ml-3">
+            <span className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
             </span>
           </Link>
         </div>
-        <div className="space x-2">
+        <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart /> Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon /> Sign in
             </Link>
---
+++ components/shared/header/mode-toggle.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState, useEffect } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuContent,
+  DropdownMenuCheckboxItem,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { SunIcon, MoonIcon, SunMoonIcon } from "lucide-react";
+
+const ModeToggle = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+  if (mounted === false) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="focus-visible:ring-0 focus-visible:ring-offset-0"
+        >
+          {theme === "dark" ? (
+            <MoonIcon />
+          ) : theme === "light" ? (
+            <SunIcon />
+          ) : (
+            <SunMoonIcon />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={theme === "system"}
+          onClick={() => setTheme("system")}
+        >
+          System
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === "dark"}
+          onClick={() => setTheme("dark")}
+        >
+          Dark
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === "light"}
+          onClick={() => setTheme("light")}
+        >
+          Light
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ModeToggle;
