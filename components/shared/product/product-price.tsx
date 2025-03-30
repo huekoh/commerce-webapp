5827aed987b3b8c2b6344fb947294f3d99eb1706
huekoh <huexkoh@gmail.com>
2025-03-30 20:37:24 +0800

    Create product price component

---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+
+const ProductPrice = ({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) => {
+  // ensure 2 decimal places
+  const stringValue = value.toFixed(2);
+
+  // get the int/float
+  const [intValue, floatValue] = stringValue.split(".");
+
+  return (
+    <p className={cn("text-2xl", className)}>
+      <span className="text-xs align-super">$</span>
+      <span className="text-2xl">{intValue}</span>
+      <span className="text-xs align-super">.{floatValue}</span>
+    </p>
+  );
+};
+
+export default ProductPrice;
